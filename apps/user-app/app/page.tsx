d92d507d53b53920dfcfd864fa34e88f99e8b70b
RishiBeesu <rishibeesu17@gmail.com>
2024-10-30 14:22:43 +0530

    bootstrapped application with tailwind and prisma

---
+++ apps/user-app/app/page.tsx
@@ -0,0 +1,11 @@
+import { PrismaClient } from "@repo/db/client";
+
+export default function Home() {
+  return (
+    <div className="flex justify-center">
+      <div className="flex flex-col justify-center h-screen">
+        <div>Hello World!!!</div>
+      </div>
+    </div>
+  );
+}
